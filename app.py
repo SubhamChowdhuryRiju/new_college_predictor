50192fd39e435a63c9ef2902827f180b41c94f42
Subham Chowdhury <109233947+SubhamChowdhuryRiju@users.noreply.github.com>
2024-04-16 01:46:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ from BackEnd.college_predictor_results import college_predictor
 
 app = Flask(__name__, template_folder='FrontEnd/templates', static_folder='FrontEnd/static')
 
-from sanic import Sanic
-from sanic.response import json
-app = Sanic()
-
 app.register_blueprint(college_predictor)
 
 
